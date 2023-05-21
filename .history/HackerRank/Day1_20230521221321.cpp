b2c742b05484edb8b92e93a729b906670491d842
WinD <duongdinh3067@gmail.com>
2023-05-21 23:19:13 +0700

    Binary Tree

--- .history/HackerRank/Day1_20230521221321.cpp
+++ .history/HackerRank/Day1_20230521221321.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main()
 {
     int i = 4;
-    double d = 4.0;
+    double d = 4;
     string s = "HackerRank ";
-    // Declare second integer, double, and String variables.
+    // Declare second integer, double, and String variables
     int a;
     double b;
     string c;
@@ -17,11 +17,12 @@ int main()
     // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
     cin >> a;
     cin >> b;
+    cin.ignore();
     getline(cin, c);
     // Print the sum of both integer variables on a new line.
     cout << i + a << endl;
     // Print the sum of the double variables on a new line.
-    cout << b + d << endl;
+    cout << d << endl;
     // Concatenate and print the String variables on a new line
     // The 's' variable above should be printed first.
     cout << s << c;
